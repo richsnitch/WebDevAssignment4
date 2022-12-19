65255506763f21e04a1c7dd6932bf25cf85f4c85
Joe Schoen <scho7642@stthomas.edu>
2022-12-18 20:05:29 -0600

    Fixed Hayden

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,7 @@ export default {
             incident: "",
             neighborhood_number: "",
             neighborhoods: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17],
+            neighborhood_names: [],
             police_grid: "",
             block: "",
             leaflet: {
